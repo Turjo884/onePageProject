66bf21f2e8c56e5f696716ed949f689a961570a6
Turjo Khan <hturjokhan@gmail.com>
2021-02-22 01:13:01 +0600

    near about done

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const hamburger = document.querySelector('.header-container .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header-container .nav-bar .nav-list ul');
-const header = document.querySelector('.header-container');
+// const hamburger = document.querySelector('.header-container .nav-bar .nav-list .hamburger');
+// const mobile_menu = document.querySelector('.header-container .nav-bar .nav-list ul');
+// const header = document.querySelector('.header-container');
 
 
-hamburger.addEventListeaner('click', () => {
-    hamburger.classList.toggle('active');
-    mobile_menu.classList.toggle('active');
-});
+// hamburger.addEventListeaner('click', () => {
+//     hamburger.classList.toggle('active');
+//     mobile_menu.classList.toggle('active');
+// });
+
+// Working Js code 
+
+const click = document.getElementById('clickTest');
+const hamShow = document.getElementById('ulShow');
+
+click.addEventListener('click',function(){
+    hamShow.classList.toggle('active');
+})
